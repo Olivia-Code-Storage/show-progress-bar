1aea5d2e304e51baf925bf21f50e1fbc74911e00
hansololiviakim <dev.hansolkim@gmail.com>
2023-09-13 00:45:17 +0900

    :sparkles: Adding style changes based on query-string

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
+import { WrapDiv, ProgressSection, PercentageDiv } from './style/AppStyle'
 
 const App = () => {
-  return <div>App</div>;
-};
+  const params = window.location.search.slice(1).split('&')
 
-export default App;
+  const [customData, setCustomData] = useState({
+    percentage: 0,
+    backgroundColor: 'transparent',
+    percentageColor: '#BB2649',
+    fontColor: 'white',
+    text: '',
+    lang: 'en',
+    noLang: 'false',
+    langColor: 'black',
+  })
+  const [done, setDone] = useState(false)
+
+  useEffect(() => {
+    for (let i of params) {
+      const [key, value] = i.split('=')
+      if (customData.hasOwnProperty(key) && value) {
+        setCustomData((prev) => ({
+          ...prev,
+          [key]: value,
+        }))
+      }
+    }
+    setDone((prev) => !prev)
+  }, [])
+
+  return (
+    done && (
+      <WrapDiv $noLang={customData.noLang} $langColor={customData.langColor}>
+        {customData.noLang === 'false' ? <span>{customData.lang === 'en' ? 'task progress' : '진행률'}</span> : ''}
+        <ProgressSection $backgroundColor={customData.backgroundColor}>
+          <PercentageDiv
+            $percentage={customData.percentage}
+            $percentageColor={customData.percentageColor}
+            $fontColor={customData.fontColor}
+          >
+            {customData.text.replaceAll(/%20/g, ' ')}
+          </PercentageDiv>
+        </ProgressSection>
+      </WrapDiv>
+    )
+  )
+}
+
+export default App
